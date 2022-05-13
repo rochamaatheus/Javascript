567bd27a622b3ae6656541732a5b17fcc691c4f1
Matheus Rocha <101670527+rochamaatheus@users.noreply.github.com>
2022-05-13 15:42:02 -0300

    aula011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou o limite de velocidade. Multado!')
+}
+console.log('Dirija sempre com cinto de segurança!')
