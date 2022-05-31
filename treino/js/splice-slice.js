f3ed31653f61cc1f0aa2e57f1b86f194e679b673
Matheus Rocha <101670527+rochamaatheus@users.noreply.github.com>
2022-05-31 20:02:32 -0300

    Create splice-slice.js

---
+++ treino/js/splice-slice.js
@@ -0,0 +1,14 @@
+const names = ['Matheus', 'Robson', 'Sandra', 'Julia']
+
+const removed = names.splice(4, 0, 'Manão', 2, 1)
+
+console.log(names)
+console.log(removed)
+console.log('------------------------------------------------')
+
+const frutas = ['Maçã', 'Banana', 'Melancia', 'Laranja', 'Limão']
+const resultado = frutas.slice(2, -2)
+
+
+console.log(frutas)
+console.log(resultado)
